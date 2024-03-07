02c8afda677899637f6be6d5dd891d4140cb4eea
kureshiasiya <kureshiasiya2@gmail.com>
2024-03-07 13:45:14 +0530

    emoji app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 //var username = prompt("Give me your name");
-var username = "Asiya";
+
 export default function App() {
+  const [likeCounter, setLikeCounter] = useState(0);
+  function likeClickHandler() {
+    var newlikeCounter = likeCounter + 1; //processing
+    setLikeCounter(newlikeCounter); //processing/output
+  }
+  console.log("likeCounter", likeCounter);
+
   return (
     <div className="App">
-      <h1 style={{ backgroundColor: "blue" }}>Welcome {username}</h1>
+      <h1>Inside Outt</h1>
+      <button onClick={likeClickHandler}>Like Me</button>
+      {likeCounter}
     </div>
   );
 }
